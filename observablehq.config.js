66e37848bf632b01eae62e8a87a49b17dbce4da5
NJ Smith <134447517+njsmithfm@users.noreply.github.com>
2025-01-22 11:32:20 -0500

    onfig footer

--- observablehq.config.js
+++ observablehq.config.js
@@ -3,6 +3,7 @@ export default {
   // The app’s title; used in the sidebar and webpage titles.
   title: "NYC Bike Crash Dashboard",
   theme: "midnight",
+  pager: "false",
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
   // lets you organize them into sections and have unlisted pages.
@@ -25,7 +26,7 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  // footer: "Built with Observable.", // what to show in the footer (HTML)
+  footer: "Built by <a href='https://linktr.ee/njsmith'>NJ Smith</a> with <a href='https://observablehq.com/'>Observable.</a>", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
